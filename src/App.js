ed5c687ecdfd7107780a161af67fcdf6672589a9
Paul-Cavain <paulcavain01@gmail.com>
2025-01-06 14:01:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Avatar from "./Avatar";
+import Intro from "./Intro";
+import SkillsList from "./SkillsList";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="App border-4 border-black w-3/4 sm:w-1/2 md:w-1/4 flex flex-col justify-center items-center p-8 bg-white rounded-lg shadow-lg">
+        <Avatar />
+        <Intro />
+        <SkillsList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+const Intro = () => {
+  return (
+    <div className="p-4">
+      <h3 className="text-2xl font-bold">Jonas Schmedtmann</h3>
+      <p className="text-justify mt-3">
+        Full stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and etc), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+};
+export default Intro;
